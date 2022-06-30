298a8f42f97f78722b72ecda81d9eb7a784d37fe
Viktor Doroshenko <reg.doroshenko@gmail.com>
2022-06-30 20:14:53 +0300

    fix to ts package

--- ts-module/build/src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.add = void 0;
-function add(x1, x2) {
-    return x1 + x2;
-}
-exports.add = add;
-//# sourceMappingURL=index.js.map
